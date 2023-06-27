878c4bbfbe4286eca9b5aeb4b684b2c41f2e14c9
johnHostetter <jhostetter16@gmail.com>
2023-06-26 21:56:30 -0400

    Update Piazza script to include date and time

--- fetch_piazza_posts.py
+++ fetch_piazza_posts.py
@@ -2,6 +2,7 @@
 This Python module contains the code to fetch posts from Piazza and store them in a Pandas
 DataFrame.
 """
+import datetime
 import pandas as pd
 
 from bs4 import BeautifulSoup
@@ -23,7 +24,7 @@ feed_filter = csc_226.get_filtered_feed(feed_filter=folder_filter)
 posts = feed_filter["feed"]
 
 # Get the post content
-results = {"id": [], "subject": [], "content": []}
+results = {"id": [], "subject": [], "content": [], "created": []}
 for post in posts:
     post_id = post["nr"]
     post_with_all_content = csc_226.get_post(
@@ -34,9 +35,14 @@ for post in posts:
     content = BeautifulSoup(
         latest_post["content"], "lxml"
     ).text  # this removes the HTML tags
+    original_date_format, new_date_format = "%Y-%m-%dT%H:%M:%SZ", "%m-%d-%Y %H:%M:%S"
+    created = datetime.datetime.strptime(
+        latest_post["created"], original_date_format
+    ).strftime(new_date_format)
     results["id"].append(post_id)
     results["subject"].append(subject)
     results["content"].append(content)
+    results["created"].append(created)
 
 # Store the results in a Pandas DataFrame
 df = pd.DataFrame(results)
