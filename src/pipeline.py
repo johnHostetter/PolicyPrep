3a380ebcea2a29df6b42a3cfb298e0635a516816
johnHostetter <jhostetter16@gmail.com>
2023-06-30 15:26:46 -0400

    Add initial pipeline entry point

---
+++ src/pipeline.py
@@ -0,0 +1,24 @@
+"""
+This module implements the pipeline for the project. The pipeline is as follows:
+    (1) Download the semester data from the Google Drive folder.
+    (2) Preprocess the data to make it compatible with InferNet.
+    (3) Aggregate the data into a single file; for example, one file for problem-level data
+    and one file for each exercises' step-level data.
+    (4) Train the InferNet models.
+"""
+
+
+from src.preprocess.data.download import download_semester_data
+from src.preprocess.data.to_infernet_format import iterate_over_semester_data
+from src.preprocess.data.aggregation import aggregate_data_for_inferring_rewards
+
+
+if __name__ == "__main__":
+    # download the data from the Google Drive folder
+    download_semester_data()
+
+    # preprocess the data to make it compatible with InferNet
+    iterate_over_semester_data()
+
+    # aggregate the data into a single file
+    aggregate_data_for_inferring_rewards()
